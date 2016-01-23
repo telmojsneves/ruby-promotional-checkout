1d5c376acbc93ebadb038e2efaf75f5f27e01dfc
thisdotrob <this.rob@icloud.com>
2016-01-23 19:37:36 +0000

    Refactor Checkout

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -14,14 +14,14 @@ class Checkout
                                   "is in an invalid format."
 
   def initialize(promotions_json)
+    raise INVALID_PROMO_RULES_MSG unless succesfully_validated(promotions_json)
+    @promo_rules = parse(promotions_json)
     @basket = {}
-    @promo_rules = parse_if_valid(promotions_json)
   end
 
   private
 
-  def parse_if_valid(promotions_json)
-    raise INVALID_PROMO_RULES_MSG unless succesfully_validated(promotions_json)
+  def parse(promotions_json)
     JSON.parse(promotions_json, symbolize_names: true)
   end
 
