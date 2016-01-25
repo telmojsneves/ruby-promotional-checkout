0f4410dbdf16ce36997763af2da04d14ba3e1710
thisdotrob <this.rob@icloud.com>
2016-01-25 17:07:44 +0000

    Refactor Checkout class

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,5 +1,5 @@
-# require 'promo_rules_parser'
-# require 'total_calculator'
+require_relative 'promo_rules_parser'
+require_relative 'total_calculator'
 
 class Checkout
 
@@ -18,24 +18,40 @@ class Checkout
     }
   }
 
-  INVALID_PRODUCT_MSG = "The provided product code does not exist"
+  INVALID_PRODUCT_MSG = "The provided product code does not exist."
 
   def initialize(promo_json, rules_parser = nil, total_calculator_klass = nil)
-    rules_parser ||= PromoRulesParser
-    total_calculator_klass ||= TotalCalculator
-    promo_rules = rules_parser::parse(promo_json)
-    @total_calculator = total_calculator_klass.new(PRODUCTS, promo_rules)
+    promo_rules = parse_promo_rules_from_json(promo_json, rules_parser)
+    initialize_total_calculator(promo_rules, total_calculator_klass)
     @basket = Hash.new(0)
   end
 
-  def scan(product_code)
-    product_code = product_code.to_sym
-    raise INVALID_PRODUCT_MSG unless PRODUCTS[product_code]
-    @basket[product_code] += 1
+  def scan(code)
+    product_exists?(code) ? add_to_basket(code) : (raise INVALID_PRODUCT_MSG)
   end
 
   def total
     @total_calculator.total(@basket)
+  end
+
+  private
+
+  def add_to_basket(product_code)
+    @basket[product_code.to_sym] += 1
+  end
+
+  def product_exists?(product_code)
+    PRODUCTS[product_code.to_sym]
+  end
+
+  def parse_promo_rules_from_json(promo_json, rules_parser)
+    rules_parser ||= PromoRulesParser
+    rules_parser::parse(promo_json)
+  end
+
+  def initialize_total_calculator(promo_rules, total_calculator_klass)
+    total_calculator_klass ||= TotalCalculator
+    @total_calculator = total_calculator_klass.new(PRODUCTS, promo_rules)
   end
 
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,15 +4,13 @@ describe Checkout do
   let(:rules_parser){ double('rules_parser', parse: test_promo_rules) }
   let(:total_calculator){ double('total_calculator', total: nil) }
   let(:total_calculator_klass){ double('total_calculator_klass', new: total_calculator) }
+
   subject { Checkout.new(test_promo_rules_json, rules_parser, total_calculator_klass) }
 
   describe '#initialize' do
-    it 'initializes an empty basket' do
-      expect(subject.instance_variable_get(:@basket)).to eq({})
-    end
-
     it 'initializes a new TotalCalculator object' do
-      expect(subject.instance_variable_get(:@total_calculator)).to eq(total_calculator)
+      expect(total_calculator_klass).to receive(:new).with(Checkout::PRODUCTS, test_promo_rules)
+      subject
     end
 
     it 'parses the provided promo rules json' do
@@ -24,21 +22,21 @@ describe Checkout do
   describe '#scan' do
     let(:expected_err_msg){ Checkout::INVALID_PRODUCT_MSG }
 
-    it 'adds a valid product to the basket' do
-      5.times { subject.scan('001') }
-      actual_basket = subject.instance_variable_get(:@basket)
-      expect(actual_basket).to eq({'001': 5})
-    end
-
     it 'raises an error if the product code is invalid' do
       expect { subject.scan('invalid') }.to raise_error(expected_err_msg)
     end
+
+    it 'adds a valid product to the basket' do
+      expect(subject).to receive(:add_to_basket)
+      subject.scan('001')
+    end
+
   end
 
   describe '#total' do
     it 'retrieves the total from the TotalCalculator object' do
-      subject.scan('002')
-      expect(total_calculator).to receive(:total).with({'002': 1})
+      5.times { subject.scan('002') }
+      expect(total_calculator).to receive(:total).with({'002': 5})
       subject.total
     end
   end
